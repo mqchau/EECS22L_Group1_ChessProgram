2057a782392bdf8f6e64cc6dc2825d34cc1a3668
Ryan Morrison <morrisor@uci.edu>
2014-01-31 23:12:07 -0800

    added loop for in gameplay screen—added ability to click and highlight squares on chessboard

--- src/View/TestDrawingFuncs.c
+++ src/View/TestDrawingFuncs.c
@@ -60,16 +60,48 @@ int main(int argc, char *argv[]){
 	  *screenMode = drawTwoPlayerMenu(window, renderer, screenMode);
 	}
 	if(*screenMode == 3){
-	  drawGameplayScreen(window, renderer);
+
+
+	  /* SDL_Renderer *gameScreen = NULL; */
+	  /* gameScreen = SDL_CreateRenderer(window, -1, 0); */
+	  /* drawGameplayScreen(window, gameScreen); */
 	  playing = 1;
+	  break;
 	}
 	if(*screenMode == 100){
 	  break;
 	}
       }    
    
+      drawGameplayScreen(window, renderer);
 
+      int x_box = 0, y_box = 0;
+      int x_pos = 0, y_pos = 0;
 
+      SDL_Event event;
+
+      while(playing){
+
+      	while(SDL_PollEvent(&event)){
+
+      	  if(event.type == SDL_MOUSEBUTTONDOWN){
+      	    if(event.button.button == SDL_BUTTON_LEFT){
+      	      x_pos = event.button.x;
+      	      y_pos = event.button.y;
+	      if(x_pos > (SCREEN_WIDTH - BOARD_WIDTH)/2 && x_pos < SCREEN_WIDTH - (SCREEN_WIDTH - BOARD_WIDTH)/2
+		 && y_pos > (SCREEN_HEIGHT - BOARD_HEIGHT)/2 && y_pos < SCREEN_HEIGHT - (SCREEN_HEIGHT - BOARD_HEIGHT)/2){
+		x_box = (x_pos/(BOARD_WIDTH/8)) * (BOARD_WIDTH/8);	    
+		y_box = (SCREEN_HEIGHT-BOARD_HEIGHT)/2 + (((BOARD_HEIGHT*y_pos)/SCREEN_HEIGHT)/75 * 75);
+		
+		drawGameplayScreen(window, renderer);
+		drawBox(renderer, x_box, y_box, BOARD_WIDTH/8, BOARD_WIDTH/8);
+	      }
+      	      playing = 0;
+      	      break;
+      	    }
+      	  }
+      	}
+      }
     /* drawOnePlayerMenu(window, renderer); */
 
     /* drawTwoPlayerMenu(window, renderer); */
@@ -86,7 +118,7 @@ int main(int argc, char *argv[]){
 
     eventMain();
 
-    SDL_RenderPresent(renderer);
+    /* SDL_RenderPresent(renderer); */
    
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
